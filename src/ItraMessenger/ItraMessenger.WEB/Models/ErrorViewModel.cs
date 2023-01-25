053e65446508620f9cda73397f3be617627e1f8b
Timofei Shchukin <42294722+timuptt@users.noreply.github.com>
2023-01-25 17:53:01 +0400

    create project

---
+++ src/ItraMessenger/ItraMessenger.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ItraMessenger.WEB.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
